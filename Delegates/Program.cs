641530fa25127bd028a4ffb6eafd0829d13329af
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-02-17 23:27:54 +0530

    [Ashish] Add:single Cast Delegate Added

---
+++ Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        public delegate int Operation(int x, int y);
+        public static int Add(int x, int y)
+        {
+            return x + y;
+
+        }
+        static void Main(string[] args)
+        {
+            Operation myobj = new Operation(Add);
+            int ans=myobj.Invoke(30,40);
+            Console.WriteLine(ans);
+           
+        }
+    }
+}
